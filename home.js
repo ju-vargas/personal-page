95735d4c6f37a57f1044444cad7cb22ff4243612
ju-vargas <ju.rodrigues0100@gmail.com>
2023-09-24 02:38:39 -0300

    uhu

--- home.js
+++ home.js
@@ -2,3 +2,17 @@ $(".square").one("mouseover", function() {
     $(this).addClass('permahover'); // Adiciona a classe apenas ao elemento atual
 });
 
+$('h1').on("mouseenter", function() {
+    $('.cursor').addClass('cursor-active'); // Adiciona a classe apenas ao elemento atual
+});
+
+$('h1').on("mouseleave", function() {
+    $('.cursor-active').removeClass('cursor-active'); // Adiciona a classe apenas ao elemento atual
+});
+
+
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px'; 
+})    
